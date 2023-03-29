1c820f6f692c1af316e6dea014c0dab65efe17f6
yc <20373113@buaa.edu.cn>
2023-03-29 23:39:52 +0800

    install Element-UI

---
+++ management/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-button @click="handleClick">Click me</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      console.log('Button clicked')
+    }
+  }
+}
+</script>
