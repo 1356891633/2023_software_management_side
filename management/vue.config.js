54a5a793fc956e3a39ffaa12606fd853e60a6cea
scrail <20373846@buaa.edu.cn>
2023-04-23 16:33:28 +0800

    proxy

--- management/vue.config.js
+++ management/vue.config.js
@@ -1,18 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
 module.exports = {
-  dev: { // ... // 跨域配置 
-    proxyTable: {
-      '/api': { 
-        // target: 'http://192.168.2.3:8888', //接口域名 
-        target: 'http://114.116.211.142:8080/api', //接口域名 
-        changeOrigin: true, //是否跨域 
-        pathRewrite: {
-          '^/api': '', //将 '/api' 替换为空 
-        },
-      },
-    }, // ... 
-  },
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://114.116.211.142:8080',  // 
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
 }
