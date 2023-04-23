0356e8a3be385482f65d86d88e33b9e609829f88
Love&peace <73578248+1356891633@users.noreply.github.com>
2023-04-23 15:17:00 +0800

    1

--- management/src/components/LoginRegister.vue
+++ management/src/components/LoginRegister.vue
@@ -150,7 +150,7 @@
     },
     methods: {
       getinfo(){
-        this.$axios.post("token-get-info-url",
+        this.$axios.post("http://114.116.211.142:8080/api/user/info",
             JSON.stringify(this.Token)).then((response) => {
                 this.ManagerUserData=response.data.user;
         });
@@ -177,7 +177,7 @@
       userLogin() {
         this.$refs["loginUser"].validate((valid) => {
           if (valid) {
-            this.$axios.post("post_url", JSON.stringify(this.loginUser)).then(response => {
+            this.$axios.post("http://114.116.211.142:8080/api/user/login", JSON.stringify(this.loginUser)).then(response => {
               if (response.code==200) {
                 this.$message({
                   message: "登录成功！",
